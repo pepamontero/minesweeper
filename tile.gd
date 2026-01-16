2240cb55e3edcdffc9eca9c5fd0e8899b4ca31f2
pepsi <pepamonterojimena@gmail.com>
2026-01-16 17:57:34 +0100

    comments

--- tile.gd
+++ tile.gd
@@ -53,6 +53,7 @@ func _on_button_gui_input(event: InputEvent) -> void:
 			if not revealed:
 				play("n0")
 			if event.button_index == MOUSE_BUTTON_LEFT:
+				# If on mouse bottom left, regardless of mode, highlight adyacent tiles of unsafe tiles
 				if revealed and not adyacents_revealed and not get_parent().is_safe(self):
 					get_parent().highlight_adyacents(self)
 		if event.is_released(): 

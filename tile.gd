aa53e87d1d330e7791e1af8aa3b50e15958fca42
pepsi <pepamonterojimena@gmail.com>
2026-01-16 17:43:42 +0100

    fixed bug: reveal handles all revealing aydacents logic (for revealed and not revealed tiles)

--- tile.gd
+++ tile.gd
@@ -52,7 +52,6 @@ func _on_button_button_down():
 		get_parent().highlight_adyacents(self)
 		
 func _on_button_button_up():
-	if not revealed:
-		reveal(get_parent().flag_mode)
+	reveal(get_parent().flag_mode)
 	if revealed and not adyacents_revealed and not get_parent().is_safe(self):
 		get_parent().unhighlight_adyacents(self)
